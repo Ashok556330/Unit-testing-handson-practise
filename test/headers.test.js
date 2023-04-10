c530aae22cca30e5b4b947b850bf38ad2bdabe11
Ashok Chakravarthy <ashok524.dev@gmail.com>
2023-04-10 17:30:46 +0530

    Added  new test case

--- test/headers.test.js
+++ test/headers.test.js
@@ -8,4 +8,20 @@ describe('loan-header', () => {
     expect(el.shadowRoot.querySelector('header').querySelectorAll('p')['0'].innerText).to.equal('Apply Loan');
   })
 
+  it('Should call localeChanged function', async () => {
+    const el = await fixture(html `<loan-header></loan-header>`);
+    const myspy = Sinon.spy(el, 'localeChanged');
+    const dummyData = {target: { id: 'en-GB' }}
+    el.localeChanged(dummyData);
+    expect(myspy.calledOnce).to.be.true;
+  })
+
+  // it('Should call localeChanged function', async () => {
+  //   const el = await fixture(html `<loan-header></loan-header>`);
+  //   let localeFunction = (el.shadowRoot.querySelectorAll('button')['en-GB']);
+  //   const func = Sinon.spy(el, "localeChanged");
+  //   localeFunction.click();
+  //   expect(func).to.be.called;
+  // })
+
 });
